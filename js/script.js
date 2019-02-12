f6b2d9c206469fd8df57459e0b6e0c9d5cc32682
Asif Rana <rasifrana@hotmail.com>
2019-02-12 23:50:14 +0000

    git initiated

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Select DOM Items
+
+var typed = new Typed(".sm-heading", {
+  strings: [
+    // "Front End Web Developer",
+    "Front End Web Developer",
+    "a programmer",
+    "html, css, javaScript, React Js, git"
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+// const imgmdl = document.querySelector(".imageModal");
+// const modal = document.querySelector(".modal");
+
+// imgmdl.addEventListener("mouseover", function() {
+//   modal.classList.add("modalAdd");
+// });
